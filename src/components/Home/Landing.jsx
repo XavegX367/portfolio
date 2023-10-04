a019dd3ceb7d73da78f5b37860894bf75e98c3c2
Joris Korsten <76949970+XavegX367@users.noreply.github.com>
2023-10-04 10:22:40 +0200

    Update Landing.jsx

--- src/components/Home/Landing.jsx
+++ src/components/Home/Landing.jsx
@@ -9,8 +9,8 @@ function Landing() {
 
             <div className="flex justify-center mt-4">
                 <span className="text-center text-sm md:text-xl mx-4 md:mx-48">
-                    I'm 21 years old and living in Schijndel, The Netherlands. I graduated from MBO Application and Media Developer level 4 in 2022. 
-                    Right now, I'm studying at Fontys Hogeschool in Eindhoven. Besides my studies, I'm also working as a junior developer at Apostle in Oss.
+                    I am 21 years old, currently residing in Schijndel, The Netherlands. I graduated from MBO Application and Media Developer level 4 in 2022. Currently, 
+                    I am pursuing further studies at Fontys Hogeschool in Eindhoven. In addition to my academic pursuits, I have a job as a junior developer at Apostle in Oss.
                 </span>
             </div>
         </div>
