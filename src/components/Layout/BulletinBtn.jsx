d7c3ce527e45a1ed18aa9254dfe3878d1d5e1afb
Joris <joris367@gmail.com>
2023-10-08 12:46:01 +0200

    fix bulletin on phone

--- src/components/Layout/BulletinBtn.jsx
+++ src/components/Layout/BulletinBtn.jsx
@@ -5,7 +5,7 @@ function BulletinBtn({name, path}) {
     const navigate = useNavigate();
     return (
         <>
-            <ul onClick={() => navigate(path)} className="flex flex-col gap-y-2 list-disc hover:list-none hover:text-black hover:bg-white rounded-full">
+            <ul onClick={() => navigate(path)} className="flex flex-col gap-y-2 list-none md:list-disc hover:list-none hover:text-black hover:bg-white rounded-full mx-4 my-4 md:mx-0 md:my-0">
                 <li 
                     className="text-center text-xl md:text-5xl md:mx-16 cursor-pointer select-none md:hover:text-6xl hover:text-2xl transition-all duration-300
                     px-4 py-2"
