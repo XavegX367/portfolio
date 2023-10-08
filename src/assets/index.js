450ecccf581a370a07a4ef3de7feb6703f15a064
Joris <joris367@gmail.com>
2023-10-08 20:40:32 +0200

    update typography section

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,11 +3,13 @@ import codigital from './images/codigital.png';
 import conference_awwwards from './images/conference_awwwards.png';
 import imarketina from './images/imarketina.png';
 import joshuas_world from './images/joshuas_world.png';
+import typography from './images/typography.png';
 
 export {
     header,
     codigital,
     conference_awwwards,
     imarketina,
-    joshuas_world
+    joshuas_world,
+    typography
 }
--- src/pages/BuildUp.jsx
+++ src/pages/BuildUp.jsx
@@ -1,4 +1,4 @@
-import { conference_awwwards, joshuas_world, codigital, imarketina } from '../assets'
+import { conference_awwwards, joshuas_world, codigital, imarketina, typography } from '../assets'
 import websites from '../components/Buildup/websites.json'
 import SiteInspiration from '../components/Buildup/SiteInspiration'
 
@@ -8,8 +8,9 @@ function BuildUp() {
         <div className='mx-4 mt-5 text-lg'>
             {/* <h2>This portfolio was made by Joris Korsten, a student at Fontys University of Applied Sciences, studying HBO ICT Media and Design in the second semester.</h2> */}
 
-            <div className='flex justify-center mt-7 flex-col'>
-                <h1 className='text-2xl text-center'>Designing the portfolio</h1>
+            <div className='flex justify-center mt-7 flex-col mb-10'>
+                <h1 className='text-2xl text-center mb-2'>Designing the portfolio</h1>
+                <h1 className='text-xl text-center'>Inspiration</h1>
                 <span className='text-center'>
                     I started gathering inspiration from various websites. As a tip, we were advised to take a look 
                     at <a href='https://www.awwwards.com/' target='blank' className='url'>Awwwards</a>. Here, 
@@ -22,6 +23,16 @@ function BuildUp() {
                     <SiteInspiration id={websites[2].id} name={websites[2].name} url={websites[2].url} img={codigital} description={websites[2].description} />
                     <SiteInspiration id={websites[3].id} name={websites[3].name} url={websites[3].url} img={imarketina} description={websites[3].description} />
                 </section>
+
+
+                <h1 className='text-xl text-center'>Typography</h1>
+                <span className='text-center'>
+                    I found the style of Imarketina very cool, which is why I decided to create my portfolio in a similar style. Now I had to find a font that matches the style of Imarketina.
+                </span>
+                <img src={typography} alt='typography' className='mx-auto mt-5 section_image mb-4' />
+                <span className='text-center mb-10'>
+                    After trying out a few fonts, LunchType22 Expanded Medium seems to be a good fit.
+                </span>
             </div>
         </div>
     )
