59148f064ce3a99f0481a08c4d59df904442f55e
Joris <joris367@gmail.com>
2023-11-27 20:14:09 +0100

    cleaned up learning outcomes

--- src/pages/LearningOutcomes.jsx
+++ src/pages/LearningOutcomes.jsx
@@ -7,12 +7,11 @@ function LearningOutcomes() {
         <div className='flex justify-left mt-7 flex-col mb-10'>
             <h1 className='text-2xl text-left mb-2'>Learning Outcomes</h1>
             <section className='flex flex-col justify-center mt-5 gap-y-5'>
-                <Outcome id={outcomes[0].id} name={outcomes[0].name} description={outcomes[0].description} prove={outcomes[0].prove} img=""/>
-                <Outcome id={outcomes[1].id} name={outcomes[1].name} description={outcomes[1].description} prove={outcomes[1].prove} img=""/>
-                <Outcome id={outcomes[2].id} name={outcomes[2].name} description={outcomes[2].description} prove={outcomes[2].prove} img=""/>
-                <Outcome id={outcomes[3].id} name={outcomes[3].name} description={outcomes[3].description} prove={outcomes[3].prove} img=""/>
-                <Outcome id={outcomes[4].id} name={outcomes[4].name} description={outcomes[4].description} prove={outcomes[4].prove} img=""/>
-                <Outcome id={outcomes[5].id} name={outcomes[5].name} description={outcomes[5].description} prove={outcomes[5].prove} img=""/>
+                {
+                    outcomes.map((outcome) => (
+                        <Outcome key={outcome.id} id={outcome.id} name={outcome.name} description={outcome.description} prove={outcome.prove} img=""/>
+                    ))
+                }
             </section>
 
         </div>
